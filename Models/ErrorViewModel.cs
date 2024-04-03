1ed9fc4ada11dbb973e26a7492a4de7d728e614e
Niva Surendran <niva.surendran@claysys.in>
2024-04-03 16:11:30 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clinic_ManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
